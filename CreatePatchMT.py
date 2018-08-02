98ceb317970f78065e37e0b3aa9988e85c42f10b
woodhfut <woodhfut@hotmail.com>
2018-08-02 18:09:41 +0800

    check file size
    check file size and capture exceptions

--- CreatePatchMT.py
+++ CreatePatchMT.py
@@ -18,7 +18,7 @@ apm_version_path = {
 
 url = 'http://rmdm-bldvm-l901:8000/sign4dev.aspx'
 account = 'qiang.liu@arcserve.com'
-password ='<PASSWORD>'
+password ='------'
 
 
 def createFix(fixname):    
@@ -122,53 +122,80 @@ def signBinary(bin):
     }
     s = requests.Session()
     s.auth =HttpNtlmAuth(account,password)
-    r = s.get(url,headers = headers )
-    print('get status code for {} is {}'.format(bin, r.status_code))
-    if r.status_code == 200:
-        soup =  BeautifulSoup(r.text,'html.parser')
-
-        vs = soup.find(id='__VIEWSTATE')['value']
-        ev = soup.find(id='__EVENTVALIDATION')['value']
-        with open(binname_dst,'rb') as f:
-            files = {'FileUpload1': f,}
-            data = {
-                '__VIEWSTATE': vs,
-                '__EVENTVALIDATION':ev,       
-                'Button1':'Upload File',
-            }
-            r = s.post(url,files = files, data=data)
-        print(r.status_code)
+    try:
+        r = s.get(url,headers = headers )
+        print('get status code for {} is {}'.format(bin, r.status_code))
         if r.status_code == 200:
-            soup = BeautifulSoup(r.text, 'html.parser')
-            links = soup.find_all('a',text='Download')
-            print('links:{}'.format(links))
-            if links and len(links) > 0:
-                link = links[0].get('href')
-                #sometimes the link is incorrect due to extra space before port number. 
-                link = link.replace(': ',':')
-                print(link)
+            soup =  BeautifulSoup(r.text,'html.parser')
 
-                with s.get(link, headers=headers, stream=True) as r:
-                #name = bin.split('\\')[-1]
-                    with open(binname_dst,'wb') as f:
-                        for chunk in r.iter_content():
-                            f.write(chunk)
-                result = True
-                #r.close()
+            vs = soup.find(id='__VIEWSTATE')['value']
+            ev = soup.find(id='__EVENTVALIDATION')['value']
+            with open(binname_dst,'rb') as f:
+                files = {'FileUpload1': f,}
+                data = {
+                    '__VIEWSTATE': vs,
+                    '__EVENTVALIDATION':ev,       
+                    'Button1':'Upload File',
+                }
+                r = s.post(url,files = files, data=data)
+            print(r.status_code)
+            if r.status_code == 200:
+                soup = BeautifulSoup(r.text, 'html.parser')
+                links = soup.find_all('a',text='Download')
+                print('links:{}'.format(links))
+                if links and len(links) > 0:
+                    link = links[0].get('href')
+                    #sometimes the link is incorrect due to extra space before port number. 
+                    link = link.replace(': ',':')
+                    print(link)
+
+                    with s.get(link, headers=headers, stream=True) as rd:
+                        total_size = 0
+                        try:
+                            total_size = int(rd.headers.get('content-length'))
+                            print('file total size of {}: {}'.format(bin,total_size))
+                            
+                        except TypeError:
+                            print('content-type is not exist, ignore the progressbar.')
+                            
+
+                        count = 0  
+                        chunk_size = 1024  
+                        downloaded_size = 0
+                        with open(binname_dst,'wb') as f:
+                            for chunk in rd.iter_content(chunk_size):
+                                if chunk: 
+                                    f.write(chunk)
+                                    if total_size != 0:
+                                        if len(chunk) == chunk_size:
+                                            count +=1
+                                            downloaded_size = count* chunk_size
+                                        else:#last chunk
+                                            downloaded_size += len(chunk)
+                        if total_size != 0:
+                            print('{}: {} of {} downloaded... '.format(bin,downloaded_size, total_size))
+
+                    result = True
+                    #r.close()
+                else:
+                    print('failed to get the download link for signed binary {}.'.format(bin))
+                    print(r.text)
+                    #exit()
+                    shutil.rmtree(temp_folder)
+                    return (temp_folder, result)
+
+                print('move signed binary back to fix path {}'.format(bin))
+                shutil.move(binname_dst, bin)
+                #shutil.rmtree(temp_folder)
             else:
-                print('failed to get the download link for signed binary {}.'.format(bin))
-                print(r.text)
-                #exit()
-                shutil.rmtree(temp_folder)
-                return (temp_folder, result)
-
-            print('move signed binary back to fix path {}'.format(bin))
-            shutil.move(binname_dst, bin)
-            shutil.rmtree(temp_folder)
+                print('failed to post data, ret={}'.format(r.status_code))
         else:
-            print('failed to post data, ret={}'.format(r.status_code))
-    else:
-        print('failed to get from {}, ret ={}'.format(url, r.status_code))
+            print('failed to get from {}, ret ={}'.format(url, r.status_code))
+    except Exception as ex:
+        print('error occurred: {}'.format(ex))
+    finally:
+        if os.path.exists(temp_folder):
+            shutil.rmtree(temp_folder)
     return (temp_folder, result)
 
 if __name__ == '__main__':
